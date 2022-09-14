fb8e55fde991ad06a4ea442ca1bd5c8de480bacc
Thomas Gander - School <Thomas.Gander@mymhc.ca>
2022-09-14 09:49:20 -0600

    Added Pictures and buttons that will show/hide depending on the button pushed

--- PictureboxDemo/Form1.cs
+++ PictureboxDemo/Form1.cs
@@ -9,17 +9,27 @@ namespace PictureboxDemo
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Code to allow a hidden picture box to show itself and hide others
             picBestGame1.Visible = true;
+            picAverageGame1.Visible = false;
+            picWorstGame1.Visible = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // Allows a value to switch
+            // picAverageGame1.Visible = !picAverageGame1.Visible;
+            picBestGame1.Visible = false;
             picAverageGame1.Visible = true;
+            picWorstGame1.Visible = false;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            picBestGame1.Visible = false;
+            picAverageGame1.Visible = false;
             picWorstGame1.Visible = true;
         }
+
     }
 }
